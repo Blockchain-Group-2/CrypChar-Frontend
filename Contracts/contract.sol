beb1550848ad6bdd3666b853febd17331062af47
GeorgeFane <39439818+GeorgeFane@users.noreply.github.com>
2020-09-09 13:02:32 -0400

    Update contract.sol

--- Contracts/contract.sol
+++ Contracts/contract.sol
@@ -1,40 +1,54 @@
 pragma solidity >=0.4.22 <0.7.0;
 
-contract CrypChar {
-
-    mapping(uint256 => uint256) public hash; //initializes a Hashtable
-    uint256 public i=0; //when first deployed, the index of the hastable is 0
-
-    function store(uint256 num) public returns (bool success){ //stores the data from a hash and increments that value
-        hash[i]+=num;
-        i++;
-        return true;
+contract MetaCoin {
+    
+    int[7] public bals;
+    
+    function getBals() public returns (int[7] memory){
+        return bals;
     }
     
-    function get(uint256 i) public view returns (uint256){ //gets data from the hash at index 'i'
-        return hash[i];
+    struct log{
+        string time;
+        address from;
+        address to;
+        uint cont;
+        int value;
+        string memo;
+        string hash;
     }
-
-    mapping (uint256 => uint256) public balances; 
-    event t(address sender, address receiver, uint continent, uint amount, bytes memo);
+    log[] public logs;
+    uint public length=0;
     
-    constructor() public{}
-
-    function transfer(address receiver, uint to, uint value, string memory memo) public payable returns (bool success) {  //transfers Ether between the donator and the charity
-        balances[to] += value; //changes the value of the balance for the charity
-        emit t(msg.sender, receiver, to, value, bytes(memo)); //emits an event logging a transaction
-        return true;
-    }
-
-    function withdraw(address receiver, uint to, uint value, string memory memo) public payable returns (bool success) {  //allows the charity to allocate money
-        if (value > balances[to]) return false; //returns false if the charity does not have enough funds to withdraw
+    function store(
+        string memory time,
+        address to,
+        uint cont,
+        int value,
+        string memory memo,
+        string memory hash
+    ) public returns (bool){
         
-        balances[to] -= value;  
-        emit t(msg.sender, receiver, to, value, bytes(memo));
+        //changes balance
+        if (value+bals[cont]<0) return false;
+        bals[cont]+=value;
+        
+        //stores faux event as struct
+        log memory t;
+        
+        t.time=time;
+        t.from=msg.sender;
+        t.to=to;
+        t.cont=cont;
+        t.value=value;
+        t.memo=memo;
+        t.hash=hash;
+        
+        logs.push(t);
+        length++;
+        
         return true;
     }
     
-    function getBalance(uint i) public payable returns (uint){  //gets the balance at the index 'i'
-        return balances[i];
-    }
+    fallback() external payable { }
 }
